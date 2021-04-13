e1bc84e46edeefe8b726d67453def4189b294a1c
Diviner <sqq_job@sina.com>
2021-04-13 23:46:50 +0800

    feat: Provider生产者源码

---
+++ 2021/Node-Library/NestJS/demo-03-provider/src/app.module.ts
@@ -0,0 +1,19 @@
+/*
+ * @Description:
+ * @Autor: Diviner
+ * @Date: 2021-04-13 21:00:22
+ * @LastEditors: Diviner
+ * @LastEditTime: 2021-04-13 23:38:33
+ */
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatsController } from './modules/cats/cats.controller';
+import { CatsService } from './modules/cats/cats.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController, CatsController],
+  providers: [AppService, CatsService],
+})
+export class AppModule {}
---
+++ 2021/Node-Library/NestJS/demo-03-provider/src/modules/cats/interface/cat.interface.ts
@@ -0,0 +1,13 @@
+/*
+ * @Description:
+ * @Autor: Diviner
+ * @Date: 2021-04-13 23:27:10
+ * @LastEditors: Diviner
+ * @LastEditTime: 2021-04-13 23:28:18
+ */
+
+export interface Cat {
+  name: string;
+  age: number;
+  breed: string;
+}
